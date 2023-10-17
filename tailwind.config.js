a6c957874e36be26ac28cd58b3c936b43043be13
Clark Rabe <45646499+ClarkRabe@users.noreply.github.com>
2023-10-17 20:16:10 +0000

    html changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = 
 {
-  content: ['./src/index.html ', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
